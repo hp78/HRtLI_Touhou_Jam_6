0ad144309681a0e02ad9d45b65b22c96613eec85
sinchronise <45794094+sinchronise@users.noreply.github.com>
2021-02-06 05:36:03 +0800

    not gonna bother with an effects manager cuz this game is shooort and im lazy

---
+++ Assets/Scripts/FXScripts/BlinkingFX.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkingFX : MonoBehaviour
+{
+    //Requires a SpriteRenderer
+    SpriteRenderer SR;
+
+    public float blinkingTime;
+    public float blinkFrequency;
+    float currTime;
+    Coroutine blinkRoutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Check for SpriteRenderer
+        if (GetComponent<SpriteRenderer>())
+        {
+            SR = GetComponent<SpriteRenderer>();
+        }
+
+        else
+        {
+            foreach (Transform child in transform)
+            {
+                if (child.gameObject.GetComponent<SpriteRenderer>())
+                {
+                    SR = (child.gameObject.GetComponent<SpriteRenderer>());
+                    break;
+                }
+            }
+        }
+
+        if(SR == null)
+        {
+            print("SpriteRenderer not found. Requires a SpriteRenderer, deleting");
+            Destroy(this);
+        }
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            StartBlink();
+        }
+
+        if(currTime < blinkingTime)
+            currTime += Time.deltaTime;
+    }
+
+    public void StartBlink()
+    {
+        currTime = 0;
+        StopCoroutine(BlinkToggle());
+        StartCoroutine(BlinkToggle());
+    }
+
+    IEnumerator BlinkToggle()
+    {
+        while(currTime < blinkingTime)
+        {
+            if (SR.enabled)
+                SR.enabled = false;
+
+            else
+                SR.enabled = true;
+
+            yield return new WaitForSeconds(blinkFrequency);
+        }
+
+        SR.enabled = true;
+
+    }
+}
---
+++ Assets/Scripts/FXScripts/FXManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXManager : MonoBehaviour
+{
+    private static FXManager _instance;
+    SquashStretch squashStretchfx;
+
+    public static FXManager Instance { get { return _instance; } }
+
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            print("Additional FXManager Found, Deleting this");
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            _instance = this;
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        squashStretchfx = GetComponent<SquashStretch>();
+    }
+
+    public void SSFX()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
