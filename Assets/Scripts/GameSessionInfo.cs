eb907a5551a2b1715edbd7babb136d4482eae5b0
hp78 <hp78rox@gmail.com>
2021-02-05 23:43:00 +0800

    Commiting regularly is good practice etc etc

---
+++ Assets/Scripts/GameSessionInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "GameSessionInfo", menuName = "Game Session", order = 51)]
+public class GameSessionInfo : ScriptableObject
+{
+    public int currLevel;
+    public bool hasCleared = false;
+
+}
