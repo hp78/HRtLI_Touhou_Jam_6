0ba5b9b4dc3e80b095c0508f47db917bf1d8fb3c
RozAraiVtuber <arairoz.vtuber@gmail.com>
2021-02-06 17:24:18 +0800

    enemies knock back + die

---
+++ Assets/Scripts/EnemyTakeDamagePhysics.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTakeDamagePhysics : MonoBehaviour
+{
+    EnemyMook enemyMook;
+    Rigidbody2D rigidbody2d;
+    public Vector2 Addforce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        enemyMook = GetComponent<EnemyMook>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator TakeDmg()
+    {
+        rigidbody2d.velocity = new Vector2();
+        Debug.Log("SHIT");
+        yield return new WaitForSeconds(0.2f);
+        rigidbody2d.AddForce(Addforce,ForceMode2D.Impulse);
+        enemyMook.ReceiveDamage();
+        yield return 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("PlayerAttackBox"))
+        {
+           StartCoroutine( TakeDmg());
+        }
+    }
+}
