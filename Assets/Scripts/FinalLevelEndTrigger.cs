7f29a15957ce9db6686594874aa3ce76f8ce1ddc
hp78 <hp78rox@gmail.com>
2021-02-07 06:42:04 +0800

    Last level trigger points added

---
+++ Assets/Scripts/FinalLevelEndTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class FinalLevelEndTrigger : MonoBehaviour
+{
+    //
+    public GameStateManager gsm;
+    public Animator rocketAnimator;
+    public string varName;
+
+    //
+    [Space(5)]
+    public bool isEndingScene = false;
+    public CinemachineVirtualCamera cvc;
+    public GameObject playerGO;
+    public Transform rocketTF;
+
+    //
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        rocketAnimator.SetBool(varName, true);
+        if(isEndingScene)
+        {
+            playerGO.SetActive(false);
+            cvc.Follow = rocketTF;
+            Invoke("ChangeLevel", 7.5f);
+        }
+    }
+
+    void ChangeLevel()
+    {
+        gsm.GoToNextLevel();
+    }
+}
