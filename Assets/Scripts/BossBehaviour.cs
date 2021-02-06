a7110e15777c78ff44b25f32394edee72caab6ca
hp78 <hp78rox@gmail.com>
2021-02-07 05:28:53 +0800

    Boss looking good

---
+++ Assets/Scripts/BossBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBehaviour : MonoBehaviour
+{
+    public Animator animator;
+    public Sprite bossSprite;
+
+    public BossMapBehaviour bmb;
+
+    public IntVal comboCount;
+    bool isHeads = false;
+    float countdown = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown < 0.0f)
+        {
+            countdown = Random.Range(1.5f, 5.5f);
+            isHeads = !isHeads;
+            animator.SetBool("Heads", isHeads);
+        }
+    }
+
+    public void BoomAt(int index)
+    {
+        bmb.BoomAtSpot(index);
+    }
+
+    public void ReceiveDamage()
+    {
+        if(comboCount.value > 99)
+        {
+            // win
+        }
+    }
+}
---
+++ Assets/Scripts/BossMapBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMapBehaviour : MonoBehaviour
+{
+    public GameObject pfBigBoom;
+    public Transform[] spots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BoomAtSpot(int spotIndex)
+    {
+        Instantiate(pfBigBoom, spots[spotIndex].position, Quaternion.identity);
+    }
+}
